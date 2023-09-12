8aee60de6cee8f9571308973c45270d0cbd7c900
spike <spikeyang.anu@gmail.com>
2023-09-12 14:25:01 +1000

    add python/generator/iterable.py

---
+++ python/generator/iterable.py
@@ -0,0 +1,43 @@
+"""
+Iterable -> Iterator
+        生成
+
+Iterator 实现了
+- __iter__
+- __next__
+
+Key idea:
+- 尽管以下代码使用了继承, 但继承b. 这体现了 Duck type.
+"""
+
+from collections.abc import Iterable, Iterator
+
+
+class MyRange(Iterable):
+    def __init__(self, lower, upper):
+        self.lower = lower
+        self.upper = upper
+
+    def __iter__(self):
+        return MyRangeIterator(self.lower, self.upper)
+
+
+class MyRangeIterator(Iterator):
+    def __init__(self, lower, upper):
+        self.current = lower
+        self.upper = upper
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current >= self.upper:
+            raise StopIteration
+        else:
+            self.current += 1
+            return self.current - 1
+
+
+my_range = MyRange(0, 5)
+for i in my_range:
+    print(i)
