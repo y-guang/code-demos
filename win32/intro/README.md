19801a6149e5435b3608d3242896a0c8be49bcbf
spike <spikeyang.anu@gmail.com>
2023-10-30 10:37:01 +1100

    add win32/intro windows_desktop

--- win32/intro/README.md
+++ win32/intro/README.md
@@ -2,6 +2,17 @@
 
 [Get Started](https://learn.microsoft.com/en-us/windows/win32/learnwin32/learn-to-program-for-windows)
 
+## content
+
+### Get Started with Win32 and C++
+
+| concept             | file                                        | section                                     |
+| ------------------- | ------------------------------------------- | ------------------------------------------- |
+| Coding Conventions  |                                             | [Coding Conventions](#coding-conventions)   |
+| Strings             | [string.cpp](.\work_with_string\string.cpp) | [Working with Strings](#strings)            |
+| wWinMain            |                                             | [wWinMain](#wwinmain)                       |
+| windows desktop app | [main.cpp](.\windows_desktop\main.cpp)      | [windows desktop app](#windows-desktop-app) |
+
 ## Coding Conventions
 
 [Coding Conventions](https://learn.microsoft.com/en-us/windows/win32/learnwin32/windows-coding-conventions)
@@ -25,3 +36,18 @@ wchar_t *str = L"hello";
 visual studio relay on BOM to detect encoding. if you save a file with UTF-8 without BOM, visual studio will treat it as ANSI. 
 
 So, if code contains utf literal, save as UTF-8 with BOM.
+
+## wWinMain
+
+```c++
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow);
+```
+
+## windows desktop app
+
+[Your First Windows Program](https://learn.microsoft.com/en-us/windows/win32/learnwin32/your-first-windows-program)
+
+The simplest windows desktop app need at least two functions:
+- `wWinMain`: the entry point.
+- `WindowProc`: the window procedure. For callback of window message.
+  - `WM_DESTROY`, `WM_PAINT` are two basic messages that need to be handled.
---
+++ win32/intro/windows_desktop/main.cpp
@@ -0,0 +1,74 @@
+﻿#ifndef UNICODE
+#define UNICODE  // ensure using unicode version of micros and funcs.
+#endif
+
+#include <windows.h>
+
+// every Windows desktop application must also have a window-procedure function
+// it handles messages sent to the window.
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    PWSTR pCmdLine, int nCmdShow) {
+    const wchar_t CLASS_NAME[] = L"sample windows class";
+
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;  // register the handler to the current instance.
+    wc.lpszClassName = CLASS_NAME;  // the name of the class.
+
+    RegisterClass(&wc);  // register the class.
+
+    // create the windows
+    HWND hwnd = CreateWindowEx(0,            // style
+                               CLASS_NAME,   // specify windows class
+                               L"桌面程序",  // windows title
+                               WS_OVERLAPPEDWINDOW,
+                               // positions and size
+                               CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                               CW_USEDEFAULT,
+                               NULL,       // parent window
+                               NULL,       // menu
+                               hInstance,  // instance handle
+                               NULL        // additional application data
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run message loop.
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(
+            &msg);  // translate virtual-key messages into character messages.
+        DispatchMessage(&msg);  // dispatch message to the window procedure.
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
+                            LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        case WM_PAINT: {
+            // re-paint the windows. if comment out this case, the windows will
+            // not be re-painted. e.g. re-size the windows.
+            PAINTSTRUCT ps;
+            HDC         hdc = BeginPaint(hwnd, &ps);
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            EndPaint(hwnd, &ps);
+        }
+            return 0;
+        default:
+            break;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
